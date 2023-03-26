76cda75986dc260aff866c05a65551f6881ffaa1
Balint Csikos <balint.csikos.priv@gmail.com>
2023-03-26 15:21:45 +0200

    Change

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,10 +7,14 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
+    "@storybook/preset-create-react-app",
+    '@storybook/addon-coverage', //👈 Registers the addon
   ],
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-webpack5"
+  },
+  refs: {
+    'storybook': { disable: true }
   }
 }
