3680f9fba461f5133f01c63456334534665bdcfa
Yvett Krilek <ykrilek@arisglobal.com>
2023-04-12 16:27:00 +0200

    added new component

---
+++ src/stories/IconButton.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './iconButton.css';
+
+/**
+ * Primary UI component for user interaction
+ */
+export const IconButton = ({ primary, backgroundColor, size, label, icon, ...props }) => {
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  return (
+    <button
+      type="button"
+      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      style={backgroundColor && { backgroundColor }}
+      {...props}
+    >
+      <i className="material-icons">{icon}</i>
+    </button>
+  );
+};
+
+IconButton.propTypes = {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary: PropTypes.bool,
+  /**
+   * What background color to use
+   */
+  backgroundColor: PropTypes.string,
+  /**
+   * How large should the button be?
+   */
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  /**
+   * Button contents
+   */
+  label: PropTypes.string.isRequired,
+  /**
+  * What icon to use
+  */
+  icon: PropTypes.string,
+  /**
+   * Optional click handler
+   */
+  onClick: PropTypes.func,
+};
+
+IconButton.defaultProps = {
+  backgroundColor: null,
+  primary: false,
+  size: 'medium',
+  onClick: undefined,
+};
